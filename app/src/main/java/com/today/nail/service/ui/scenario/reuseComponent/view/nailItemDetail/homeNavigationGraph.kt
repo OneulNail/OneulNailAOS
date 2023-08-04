b6d912a7cbb6622291f8bb29ffca85591ea5be90
illustermin <134802163+illustermin@users.noreply.github.com>
2023-08-05 01:20:12 +0900

    [0.0.1][DetailView 구조수정][illustermin]
    참고해서 전체적인 틀 수정하고, 예약 달력 추가했습니다 확인부탁드려요!

---
+++ app/src/main/java/com/today/nail/service/ui/scenario/reuseComponent/view/nailItemDetail/homeNavigationGraph.kt
@@ -0,0 +1,63 @@
+package com.today.nail.service.ui.scenario.home.navigationGraph
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.navigation
+import com.autocrypt.move.kcallpax.extension.fadeComposable
+
+import com.today.nail.service.ui.TopLevelNavigationRoutes
+import com.today.nail.service.ui.TopLevelViewModel
+import com.today.nail.service.ui.scenario.home.view.BannerScreen
+import com.today.nail.service.ui.scenario.home.view.CategoryItemScreen
+import com.today.nail.service.ui.scenario.home.view.HomeScreen
+import com.today.nail.service.ui.scenario.reuseComponent.view.nailItemDetail.ItemDetailScreen
+import com.today.nail.service.ui.scenario.reuseComponent.view.nailItemDetail.ReservationScreen
+
+fun NavGraphBuilder.homeNavigationGraphWithFade(
+    navHostController: NavHostController,
+    activityViewModel: TopLevelViewModel
+) {
+    val fadeDuration = 300
+    val slideDuration = 400
+    navigation(
+        route = TopLevelNavigationRoutes.HomeGraph.routes,
+        startDestination = HomeRoute.Home.routes
+    ) {
+        fadeComposable(
+            duration = fadeDuration,
+            route = HomeRoute.Home.routes,
+        ) {
+            HomeScreen(navController = navHostController)
+        }
+
+        fadeComposable(
+            duration = fadeDuration,
+            route = HomeRoute.Banner.routes,
+        ) {
+            BannerScreen(navController = navHostController)
+        }
+
+        fadeComposable(
+            duration = fadeDuration,
+            route = HomeRoute.CategoryItem.routes,
+        ) {
+            CategoryItemScreen(navController = navHostController)
+        }
+
+        fadeComposable(
+            duration = fadeDuration,
+            HomeRoute.ItemDetail.routes
+        ) {
+            ItemDetailScreen(onCall  = {}, onInquire  = {}
+            ) {}
+        }
+        fadeComposable(
+            duration = fadeDuration,
+            HomeRoute.Reservation.routes
+        ) {
+            ReservationScreen(navController = navHostController,  onReservation ={},
+                onBack ={},)
+        }
+    }
+
+}
