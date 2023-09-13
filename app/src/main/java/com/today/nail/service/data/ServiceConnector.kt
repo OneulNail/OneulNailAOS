63cddeb1fc4fb147f6f1179bcfa7606b43248569
brightalot <gamja9151@hanyang.ac.kr>
2023-09-13 21:26:41 +0900

    [0.0.1][fix][brightalot]

--- app/src/main/java/com/today/nail/service/data/ServiceConnector.kt
+++ app/src/main/java/com/today/nail/service/data/ServiceConnector.kt
@@ -1,9 +1,12 @@
 package com.today.nail.service.data
 
 import android.util.Log
+import com.today.nail.service.config.MainApplication
 import com.today.nail.service.data.home.service.HomeService
 import com.today.nail.service.data.onBoard.service.OnBoardService
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -11,54 +14,34 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceConnector {
 
+    val interceptor = httpLoggingInterceptor()
+
+    val client = interceptor.let {
+        OkHttpClient.Builder()
+            .addInterceptor(it)
+            .build()
+    }
+
+    val retrofit = client.let {
+        Retrofit.Builder()
+            .baseUrl("http://43.201.171.215:8080/")
+            .client(it)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
     fun makeOnBoardService() : OnBoardService {
-        val interceptor = httpLoggingInterceptor()
-
-        val client = interceptor?.let {
-            OkHttpClient.Builder()
-                .addInterceptor(it)
-                .build()
-        }
-
-        val retrofit = client?.let {
-            Retrofit.Builder()
-                .baseUrl("http://43.201.171.215:8080/")
-                .client(it)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-        }
-
-        val onBoardService = retrofit?.create(OnBoardService::class.java)
-
+        val onBoardService = retrofit.create(OnBoardService::class.java)
         return onBoardService ?: throw IllegalAccessException("retrofit cannot be null")
     }
     fun makeHomeService() : HomeService {
-        val interceptor = httpLoggingInterceptor()
-
-        val client = interceptor?.let {
-            OkHttpClient.Builder()
-                .addInterceptor(it)
-                .build()
-        }
-
-        val retrofit = client?.let {
-            Retrofit.Builder()
-                .baseUrl("http://43.201.171.215:8080/")
-                .client(it)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-        }
-
-        val homeService = retrofit?.create(HomeService::class.java)
-
+        val homeService = retrofit.create(HomeService::class.java)
         return homeService ?: throw IllegalAccessException("retrofit cannot be null")
     }
 
-    private fun httpLoggingInterceptor(): HttpLoggingInterceptor? {
+    private fun httpLoggingInterceptor(): HttpLoggingInterceptor {
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = HttpLoggingInterceptor.Level.BODY
         return interceptor
     }
-
 
 }
--- app/src/main/java/com/today/nail/service/data/home/dto/reservation/ReservationReqDTO.kt
+++ app/src/main/java/com/today/nail/service/data/home/dto/reservation/ReservationReqDTO.kt
@@ -1,7 +1,10 @@
 package com.today.nail.service.data.home.dto.reservation
 
+import android.content.Context
+import com.today.nail.service.data.TokenSharedPreferences
 import kotlinx.serialization.Serializable
 import java.time.LocalDateTime
+import kotlin.coroutines.coroutineContext
 
 @Serializable
 data class ReservationReqDTO(
